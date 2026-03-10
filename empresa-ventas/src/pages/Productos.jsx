8244de9d9c34e2d86328e8a3db596be231b282e0
Compu5 <giovanni.sanchez.roman@ctpcit.ed.cr>
2026-03-10 07:08:06 -0600

    String que usas en el products fran

--- empresa-ventas/src/pages/Productos.jsx
+++ empresa-ventas/src/pages/Productos.jsx
@@ -1,6 +1,12 @@
 import Button from "../components/CustomButton";
 import "../assets/css/productos.css"
 function Productos(){
+    const [product, setProduct] = useState({
+        name: "Producto 1",
+        description: "Descripción del producto 1",
+        price: "$10.00",
+        img: "https://via.placeholder.com/150"
+    });
     return(
         <div id="box">
             <h1>Productos</h1>
