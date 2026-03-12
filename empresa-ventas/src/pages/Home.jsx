2ea21e1c66629d5ad17300c1a86acf4952107603
Compu5 <giovanni.sanchez.roman@ctpcit.ed.cr>
2026-03-12 10:54:47 -0600

    Cambios de arreglo

--- empresa-ventas/src/pages/Home.jsx
+++ empresa-ventas/src/pages/Home.jsx
@@ -4,7 +4,8 @@ import "../assets/css/home.css";
 import img1 from"../assets/img/arImg5.jpeg";
 import Modal from "../components/Modal";
 import ScrollVelocity from '../components/ScrollVelocity';
-  
+import Carousel from './Carousel'
+
 
 
 
@@ -63,6 +64,9 @@ function Home() {
                 velocity={50}
                 className="custom-scroll-text"
                 />
+            </section>
+            <section>
+                
             </section>
         </div>
     );
