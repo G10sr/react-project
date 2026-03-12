0b4937524c1ad045c779fd7682c71a96abad2cf6
Compu5 <giovanni.sanchez.roman@ctpcit.ed.cr>
2026-03-12 12:19:00 -0600

    Fixing github idk

--- empresa-ventas/src/pages/Productos.jsx
+++ empresa-ventas/src/pages/Productos.jsx
@@ -1,1 +1,17 @@
-src/pages/Productos.jsx
+import products from "../data/products";
+import ProductList from "../components/ProductList";
+import "../assets/css/productos.css";
+ 
+function Productos() {
+  return (
+    <div id="box">
+      <h1>Productos</h1>
+ 
+      <ProductList title="Almuerzos" items={products.almuerzos} />
+      <ProductList title="Bebidas" items={products.bebidas} />
+      <ProductList title="Postres" items={products.postres} />
+    </div>
+  );
+}
+ 
+export default Productos;
