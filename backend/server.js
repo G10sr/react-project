97a8f4b2bcc94172da5348fb1a7398085b95a1d8
Compu5 <giovanni.sanchez.roman@ctpcit.ed.cr>
2026-03-17 07:19:20 -0600

    Applisten

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,14 @@ const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+//Aqui va la base de datos
+
+//Aqui va el app listen
+
+app.listen(3001, () => {
+    console.log("Servidor backend funcionando en puerto 3001")
+})
