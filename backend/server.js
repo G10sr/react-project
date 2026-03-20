e496c20b8f25b5ba82d0d48f119fe5641ffa2421
Compu5 <giovanni.sanchez.roman@ctpcit.ed.cr>
2026-03-20 06:39:52 -0600

    Avance?

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
+const pool = require("./db");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post("/carrito", async (req, res) => {
+  const { nombre, precio, imagen } = req.body;
 
-//Aqui va la base de datos
+  if (!nombre || !precio || !imagen) {
+    return res.status(400).json({ error: "Faltan datos" });
+  }
 
-//Aqui va el app listen
+  try {
+    const result = await pool.query(
+      `INSERT INTO carrito (nombre, precio, imagen, cantidad)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [nombre, precio, imagen, 1]
+    );
+
+    res.json(result.rows[0]);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error al agregar producto" });
+  }
+});
+
 
 app.listen(3001, () => {
-    console.log("Servidor backend funcionando en puerto 3001")
-})
+    console.log("Servidor backend funcionando en puerto 3001");
+});
